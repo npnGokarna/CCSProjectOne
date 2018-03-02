c028200e943a934c34c4c3efeb9dc103d9d44b8f
Gokarna Neupane <npn.gokarna@gmail.com>
2018-03-02 15:11:38 -0800

    Oh i realize my mistake now

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+       $(document).ready(function(){
+           $("#login").load("login.html");
+           $("#navbar").load("navbar.html");
+           $("#signup").load("signup.html");
+           $("#forgetpassword").load("forgetpassword.html");
+
+       });
